9676b7d1a3b3a70adf793b2c6b1caf3e87c1d26d
ramakrishnadas <rama.k.d.b.p@gmail.com>
2023-06-15 13:30:09 -0400

    completed shapes assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,23 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Square mySquare = new Square("Red", 3);
+        Rectangle myRectangle = new Rectangle("Blue", 4, 5);
+        Circle myCircle = new Circle("Green", 6);
+        
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(mySquare);
+        shapes.Add(myRectangle);
+        shapes.Add(myCircle);
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"The {s.GetColor()} shape has an area of {s.GetArea()}.");
+        }
+
     }
 }
